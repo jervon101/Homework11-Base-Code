bcb35bcbdf9b3b55ffa72e785fb8ea54cd2ae6cb
Jervon Arnold <jervonjarnold@gmail.com>
2017-12-01 12:54:57 -0500

    second commit

--- app/src/main/java/com/example/rusili/homework11/detailscreen/model/objects/Sprites.java
+++ app/src/main/java/com/example/rusili/homework11/detailscreen/model/objects/Sprites.java
@@ -10,6 +10,30 @@ public class Sprites {
 	private String front_default;
 	private String front_shiny;
 
+	public String getBack_female() {
+		return back_female;
+	}
+
+	public String getBack_shiny_female() {
+		return back_shiny_female;
+	}
+
+	public String getFront_female() {
+		return front_female;
+	}
+
+	public String getFront_shiny_female() {
+		return front_shiny_female;
+	}
+
+	public String getBack_shiny() {
+		return back_shiny;
+	}
+
+	public String getFront_shiny() {
+		return front_shiny;
+	}
+
 	public String getBack_default () {
 		return back_default;
 	}
--- app/src/main/java/com/example/rusili/homework11/pokedexActivity/view/PokedexFragment.java
+++ app/src/main/java/com/example/rusili/homework11/pokedexActivity/view/PokedexFragment.java
@@ -1,10 +1,13 @@
 package com.example.rusili.homework11.pokedexActivity.view;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -15,6 +18,7 @@ import android.app.Fragment;
 
 import com.example.rusili.homework11.PokemonAdapter;
 import com.example.rusili.homework11.R;
+import com.example.rusili.homework11.detailscreen.view.PokemonDetailActivity;
 import com.example.rusili.homework11.network.RetrofitFactory;
 import com.example.rusili.homework11.pokedexActivity.model.Pokedex;
 import com.example.rusili.homework11.pokedexActivity.model.objects.PokemonEntries;
@@ -31,8 +35,10 @@ public class PokedexFragment extends Fragment {
 
     Context context;
     RecyclerView recyclerView;
+    Intent intent;
 
-//    final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
+    private static final String SHARED_PREFS_KEY = "notes";
+    private SharedPreferences log;
 
     private RetrofitFactory.PokedexNetworkListener pokedexNetworkListener;
 
@@ -42,10 +48,14 @@ public class PokedexFragment extends Fragment {
         View view = inflater.inflate(R.layout.cycle_view, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.recycle_view);
         getPokedexList();
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
+        intent = new Intent(container.getContext(), PokemonDetailActivity.class);
 
-        recyclerView.setLayoutManager(linearLayoutManager);
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
 
+//        recyclerView.setLayoutManager(linearLayoutManager);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(context, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
 
         return view;
 
@@ -55,7 +65,7 @@ public class PokedexFragment extends Fragment {
         pokedexNetworkListener = new RetrofitFactory.PokedexNetworkListener() {
             @Override
             public void pokedexCallback(Pokedex pokedex) {
-List<PokemonEntries> entriesList = new ArrayList<>();
+                List<PokemonEntries> entriesList = new ArrayList<>();
                 // TODO: show Pokemon
                 // Each pokemon is in the Pokemon_Species object.
 
@@ -63,13 +73,30 @@ List<PokemonEntries> entriesList = new ArrayList<>();
                 PokemonEntries[] entries = pokedex.getPokemon_entries();
 
 
+
                 Log.e("Lenght ", entries.length + "");
 
 
 //                List<PokemonEntries> entriesList = new ArrayList<>(Arrays.asList(entries));
 
-                for (int i = 0; i < 151 ; i++) {
-                     entriesList.add(pokedex.getPokemon_entries()[i]);
+
+                for (int i = 0; i < entries.length; i++) {
+
+//                    String url = log.getString(pokedex.getPokemon_entries()[i].getPokemon_species().getName(), null);
+
+//                    pokedex.getPokemon_entries()[i].getPokemon_species().setUrl(url);
+
+                    entriesList.add(pokedex.getPokemon_entries()[i]);
+//name
+
+
+
+
+
+                    intent.putExtra("test", true);
+                    intent.putExtra("1", pokedex.getPokemon_entries()[i].getPokemon_species().getName());
+
+
 //                    Log.e("Lenght of list ", pokedex.getPokemon_entries()[i].getPokemon_species().getName()+ "");
                 }
 
