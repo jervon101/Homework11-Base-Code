97392cd6ed91d6175670f729edf175130f5bc48b
Jervon Arnold <jervonjarnold@gmail.com>
2017-12-02 12:25:34 -0500

    Added working app with cycle view and detail list, images can be see on the cycle view

--- app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
+++ app/src/main/java/com/example/rusili/homework11/detailscreen/view/PokemonDetailActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -22,7 +23,7 @@ import com.squareup.picasso.Picasso;
 public class PokemonDetailActivity extends AppCompatActivity {
     private RetrofitFactory.PokemonNetworkListener pokemonNetworkListener;
 
-
+    RecyclerView recyclerView;
     Boolean bol;
     TextView name, type1, type2;
     ImageView view1;
@@ -76,8 +77,10 @@ public class PokemonDetailActivity extends AppCompatActivity {
                 //TODO: Display pokemon data
 
                 name.setText(id.toUpperCase());
+
+
                 Picasso.with(getApplicationContext())
-                        .load(pokemon.getSprites().getFront_default())
+                        .load(pokemon.getSprites().getFront_default()).centerCrop().fit()
                         .into((ImageView) findViewById(R.id.image1));
 
 //                Glide.with(getApplicationContext())
@@ -85,47 +88,127 @@ public class PokemonDetailActivity extends AppCompatActivity {
 //                        .into(view1);
 
 
-                Log.e("Tesr", pokemon.getTypes().length + "");
-
-
-
-                    if (pokemon.getTypes()[0] != null) {
-                        type1.setText(pokemon.getTypes()[0].getType().getName());
-                    }
-                    if (pokemon.getTypes().length >= 2) {
-                        type2.setText(pokemon.getTypes()[1].getType().getName());
-                    }
-
-
-
-                for (int i = 0; i < pokemon.getStats().length; i++) {
-                    switch (i) {
-                        case 0:
-                            pokemon.getStats()[i].getStat().getName();
-                            speed.setProgress(pokemon.getStats()[i].getBase_stat());
+                if (pokemon.getTypes()[0] != null) {
+                    type1.setText(pokemon.getTypes()[0].getType().getName());
+                    switch (pokemon.getTypes()[0].getType().getName()) {
+                        case "fire":
+                            type1.setBackgroundColor(getResources().getColor(R.color.fire));
                             break;
-                        case 1:
-                            pokemon.getStats()[i].getStat().getName();
-                            special_dk.setProgress(pokemon.getStats()[i].getBase_stat());
+                        case "water":
+                            type1.setBackgroundColor(getResources().getColor(R.color.water));
                             break;
-                        case 2:
-                            pokemon.getStats()[i].getStat().getName();
-                            special_ak.setProgress(pokemon.getStats()[i].getBase_stat());
+                        case "poison":
+                            type1.setBackgroundColor(getResources().getColor(R.color.poison));
                             break;
-                        case 3:
-                            pokemon.getStats()[i].getStat().getName();
-                            def.setProgress(pokemon.getStats()[i].getBase_stat());
+                        case "bug":
+                            type1.setBackgroundColor(getResources().getColor(R.color.bug));
                             break;
-                        case 4:
-                            pokemon.getStats()[i].getStat().getName();
-                            ak.setProgress(pokemon.getStats()[i].getBase_stat());
+                        case "ground":
+                            type1.setBackgroundColor(getResources().getColor(R.color.ground));
                             break;
-                        case 5:
-                            pokemon.getStats()[i].getStat().getName();
-                            hp.setProgress(pokemon.getStats()[i].getBase_stat());
+                        case "electric":
+                            type1.setBackgroundColor(getResources().getColor(R.color.electric));
+                            break;
+                        case "normal":
+                            type1.setBackgroundColor(getResources().getColor(R.color.normal));
+                            break;
+                        case "fairy":
+                            type1.setBackgroundColor(getResources().getColor(R.color.fairy));
+                            break;
+                        case "fighting":
+                            type1.setBackgroundColor(getResources().getColor(R.color.fighting));
+                            break;
+                        case "psychic":
+                            type1.setBackgroundColor(getResources().getColor(R.color.psychic));
+                            break;
+                        case "steel":
+                            type1.setBackgroundColor(getResources().getColor(R.color.steel));
+                            break;
+                        case "ice":
+                            type1.setBackgroundColor(getResources().getColor(R.color.ice));
+                            break;
+                        case "ghost":
+                            type1.setBackgroundColor(getResources().getColor(R.color.ghost));
+                            break;
+                        case "rock":
+                            type1.setBackgroundColor(getResources().getColor(R.color.rock));
+                            break;
+                        case "dragon":
+                            type1.setBackgroundColor(getResources().getColor(R.color.dragon));
+                            break;
+                        case "flying":
+                            type1.setBackgroundColor(getResources().getColor(R.color.flying));
                             break;
                     }
                 }
+
+
+                if (pokemon.getTypes().length >= 2) {
+                    type2.setText(pokemon.getTypes()[1].getType().getName());
+                    switch (pokemon.getTypes()[1].getType().getName()) {
+                        case "fire":
+                            type2.setBackgroundColor(getResources().getColor(R.color.fire));
+                            break;
+                        case "water":
+                            type2.setBackgroundColor(getResources().getColor(R.color.water));
+                            break;
+                        case "poison":
+                            type2.setBackgroundColor(getResources().getColor(R.color.poison));
+                            break;
+                        case "bug":
+                            type2.setBackgroundColor(getResources().getColor(R.color.bug));
+                            break;
+                        case "ground":
+                            type2.setBackgroundColor(getResources().getColor(R.color.ground));
+                            break;
+                        case "electric":
+                            type2.setBackgroundColor(getResources().getColor(R.color.electric));
+                            break;
+                        case "normal":
+                            type2.setBackgroundColor(getResources().getColor(R.color.normal));
+                            break;
+                        case "fairy":
+                            type2.setBackgroundColor(getResources().getColor(R.color.fairy));
+                            break;
+                        case "fighting":
+                            type2.setBackgroundColor(getResources().getColor(R.color.fighting));
+                            break;
+                        case "psychic":
+                            type2.setBackgroundColor(getResources().getColor(R.color.psychic));
+                            break;
+                        case "steel":
+                            type2.setBackgroundColor(getResources().getColor(R.color.steel));
+                            break;
+                        case "ice":
+                            type2.setBackgroundColor(getResources().getColor(R.color.ice));
+                            break;
+                        case "ghost":
+                            type2.setBackgroundColor(getResources().getColor(R.color.ghost));
+                            break;
+                        case "rock":
+                            type2.setBackgroundColor(getResources().getColor(R.color.rock));
+                            break;
+                        case "dragon":
+                            type2.setBackgroundColor(getResources().getColor(R.color.dragon));
+                            break;
+                        case "flying":
+                            type2.setBackgroundColor(getResources().getColor(R.color.flying));
+                            break;
+                        case "grass":
+                            type2.setBackgroundColor(getResources().getColor(R.color.grass));
+                            break;
+                    }
+                }
+
+
+                speed.setProgress(pokemon.getStats()[0].getBase_stat());
+                special_dk.setProgress(pokemon.getStats()[1].getBase_stat());
+                special_ak.setProgress(pokemon.getStats()[2].getBase_stat());
+                def.setProgress(pokemon.getStats()[3].getBase_stat());
+                ak.setProgress(pokemon.getStats()[4].getBase_stat());
+                hp.setProgress(pokemon.getStats()[5].getBase_stat());
+
+
             }
         };
 
